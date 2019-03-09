ffd728c350897deca2174f1b821e0b1a8823702e
Ankit Jain <anki2jain@gmail.com>
2019-03-09 18:09:24 +0530

    removal of .idea from git

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_09_043644) do
+ActiveRecord::Schema.define(version: 2019_03_09_081627) do
 
   create_table "applications", force: :cascade do |t|
     t.text "answer"
